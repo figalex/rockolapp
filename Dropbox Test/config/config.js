141251762d448ef5032e95f2f15e8a144443bc20
Alejandro Figueroa <afigueroa@arkusnexus.com>
2014-03-16 09:41:41 -0700

    First dropbox API interaction testing

---
+++ Dropbox Test/config/config.js
@@ -0,0 +1,34 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'rockolapp'
+    },
+    port: 3000,
+    db: 'mysql://root@localhost/rockolapp1'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'rockolapp'
+    },
+    port: 3000,
+    db: 'mysql://root@localhost/rockolapp_test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'rockolapp'
+    },
+    port: 3000,
+    db: 'mysql://root@localhost/rockolapp_production'
+  }
+};
+
+module.exports = config[env];
