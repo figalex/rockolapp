77fff9177f80906361eaf6196289ac8e001a9e08
Alejandro Figueroa <afigueroa@arkusnexus.com>
2014-03-18 11:17:16 -0700

    Music player capability added

---
+++ Dropbox Test/public/js/main.js
@@ -0,0 +1,24 @@
+/* Logic to play a song */
+
+function playSong(item){
+	var songName = item.innerHTML;
+	$.getJSON('/song/'+songName, function(url){
+		var player = document.getElementById('player');
+		while(player.hasChildNodes()){
+			player.removeChild(player.lastChild);
+		}
+
+		var playlist = [{
+			mp3: url.data,
+			rating:0,
+			title:songName,
+			duration: '3:30',
+			artist: 'AC/DC',
+		}];
+
+		$('#player').ttwMusicPlayer(playlist, {
+			autoPlay:true,
+			description:''
+		});
+	});
+};
