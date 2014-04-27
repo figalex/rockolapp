8ddb4f30e76bc04d1b357b527b3077706cd45842
Alejandro Figueroa <afigueroa@arkusnexus.com>
2014-04-27 12:33:25 -0700

    New way to handle dropbox link

--- Dropbox Test/app/controllers/auth.js
+++ Dropbox Test/app/controllers/auth.js
@@ -1,134 +1,135 @@
 var Dropbox = require('dropbox'),
-	songsTasks = require('./songs'),
-	usersTasks = require('./users');
+    songsTasks = require('./songs'),
+    usersTasks = require('./users');
 
 var client = new Dropbox.Client({
-	key:'mqxinfiibo2kq4t',
-	secret:'tcnknprauyc9ime'
+    key: 'mqxinfiibo2kq4t',
+    secret: 'tcnknprauyc9ime'
 });
 
 var user = {},
-	songs = [];
+    songs = [];
 
-exports.login = function(req, res){
-	client.authDriver(new Dropbox.AuthDriver.NodeServer({port:8191}));
+exports.login = function(req, res) {
+    client.authDriver(new Dropbox.AuthDriver.NodeServer({
+        port: 8191
+    }));
 
-	client.authenticate(function(error, authClient){
-		if(error) throw new Error(error);
-		console.log('CLIENT AUTHENTICATED');
-		console.log(authClient);
-		client = authClient;
-		user.token = authClient._oauth._token;
+    client.authenticate(function(error, authClient) {
+        if (error) throw new Error(error);
+        console.log('CLIENT AUTHENTICATED');
+        console.log(authClient);
+        client = authClient;
+        user.token = authClient._oauth._token;
 
-		client.getAccountInfo(function(error, info){
-			if(error) throw new Error(error);
-			console.log('User info:');
-			console.log(info);
-			user.name = info.name;
-			user.email = info.email;
+        client.getAccountInfo(function(error, info) {
+            if (error) throw new Error(error);
+            console.log('User info:');
+            console.log(info);
+            user.name = info.name;
+            user.email = info.email;
 
-			usersTasks.create(user, function(err, newUser){
-				if(err) throw new Error();
+            usersTasks.lookUp(user, function(err, userFound) {
+                if (err) throw new Error();
 
-				user = newUser;
-			});
-		});
+                if (!userFound) {
+                    usersTasks.create(user, function(err, newUser) {
+                        if (err) throw new Error();
 
-		res.redirect('/scan');
-	});
+                        user = newUser;
+                    });
+                } else {
+                    user = userFound.info;
+                    songs = userFound.songs;
+                }
+            });
+        });
+
+        res.redirect('/scan');
+    });
 };
 
-exports.scan = function(req, res){
-	res.render('home/scan', {});
+exports.scan = function(req, res) {
+    res.render('home/scan', {});
 };
 
 
-exports.app = function(req,res){
+exports.app = function(req, res) {
+    debugger;
+    if (songs.length < 1) {
 
-	client.search('/', 'Rockolapp', {limit:10 } ,function(err, results){
-		if(err) throw new Error();
+        client.search('/', 'Rockolapp', {
+            limit: 10
+        }, function(err, results) {
+            debugger;
+            if (err) {
+                res.redirect('/');
+                return;
+            }
 
-		var folderFound = false, mp3s = [];
-		
-		if(results.length >  0)
-		{
-			for(var i=0;i<results.length && !folderFound;i++)
-			{
-				folderFound = (results[i].isFolder && results[i].path == '/Rockolapp');
-				console.log(results[i]);
-			}
-		}
-		console.log('Folder found: ');
-		console.log(folderFound);
-		if(folderFound){
-			client.readdir('/Rockolapp', function(err, entriesString, dirStatus, entriesStatus){
-				if(err) throw new Error(err);
+            var folderFound = false,
+                mp3s = [];
 
-				if(entriesStatus.length > 0)
-				{
+            if (results.length > 0) {
+                for (var i = 0; i < results.length && !folderFound; i++) {
+                    folderFound = (results[i].isFolder && results[i].path == '/Rockolapp');
+                }
+            }
 
-					entriesStatus.forEach(function(entrie){
-						songsTasks.saveSong(entrie, user, client, function(err, result){
-							if(err) throw new Error(err);
-							songs.push(result);
-						});
-					});
-				}
-				else
-				{
-					console.log(' NO SONGS');
-				}
+            if (folderFound) {
+                client.readdir('/Rockolapp', function(err, entriesString, dirStatus, entriesStatus) {
+                    if (err) throw new Error(err);
 
-				res.render('index', {
-					user: user,
-					songs: songs
-				});
-			});
-		}
-		else
-		{
-			client.mkdir('/Rockolapp', function(err, folderStat){
-				if(err) throw new Error(err);
+                    if (entriesStatus.length > 0) {
 
-				console.log('Folder Rockolapp created');
-				res.render('index', {
-					user: user,
-					songs: songs
-				});
-			});
-		}
-	});
+                        entriesStatus.forEach(function(entrie) {
+                            songsTasks.saveSong(entrie, user, client, function(err, result) {
+                                if (err) throw new Error(err);
+                                songs.push(result);
 
+                                if (songs.length == entriesStatus.length) {
+                                    res.render('index', {
+                                        user: user,
+                                        songs: songs
+                                    });
+                                }
+                            });
+                        });
+                    } else {
+                        console.log(' NO SONGS');
+                        res.render('index', {
+                            user: user,
+                            songs: songs
+                        });
+                    }
+                });
+            } else {
+                client.mkdir('/Rockolapp', function(err, folderStat) {
+                    if (err) throw new Error(err);
 
-	// client.getAccountInfo(function(error, info){
-	// 	if(error) throw new Error(error);
-	// 	console.log('User info:');
-	// 	console.log(info);
-	// 	userName = info.name;
-	// 	client.readdir("/Rockolapp", function(error, entries){
-	// 		if(error) {
-	// 			console.log(error);
-	// 			throw new Error(error);
-	// 		}
-	// 		console.log('Entries');
-	// 		console.log(entries);
-	// 		songs = entries;
-	// 		res.render('index', {
-	// 			user: userName,
-	// 			songs: entries
-	// 		});
-	// 	});
-	// });
+                    console.log('Folder Rockolapp created');
+                    res.render('index', {
+                        user: user,
+                        songs: songs
+                    });
+                });
+            }
+        });
+    } else {
+        res.render('index', {
+            user: user,
+            songs: songs
+        });
+    }
 };
 
-exports.song = function(req, res){
-	var songID = req.params.songName;
-	console.log(songID);
+exports.song = function(req, res) {
+    var songID = req.params.songName;
+    console.log(songID);
 
-	songsTasks.get(songID, function(err, songObj){
-		if(err) throw new Error();
+    songsTasks.get(songID, function(err, songObj) {
+        if (err) throw new Error();
 
-		res.send(songObj);
-	});
+        res.send(songObj);
+    });
 };
-
